356218bc804ffb7a94de345b38e7b6fe5e92ae12
Suraj <rothesuraj7@gmail.com>
2020-12-06 20:41:30 +0530

    Initial Commit

--- MAC/settings.py
+++ MAC/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '$tvcez*rog0ra%14n#h9(8r^^qpiw8j)z41$%4x1n!drbz%gwj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'macshop.herokuapp.com/']
 
 
 # Application definition
